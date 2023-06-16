dff38056f43d76e10f975fb62199089c64d6a47b
Abeloub <bel2ayoub@gmail.com>
2023-06-15 17:09:56 -0700

    1st level programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - entre point
- * Description : print with puts comand
+ * Description : print with printf comand
  * Return: 0
 */
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
