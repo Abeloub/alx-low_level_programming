19f0bc6e7a7bf93366d7ef31bfc5a0070b541ff6
Abeloub <bel2ayoub@gmail.com>
2023-07-27 17:14:20 -0700

    linked lists

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: head of the list
+ */
+void free_list(list_t *head)
+{
+	if (head)
+	{
+		if (head->str)
+			free(head->str);
+		free_list(head->next);
+		free(head);
+	}
+}
