ee2fcafc860f6a1bcc29da5b391a30a7e6baa999
Abel <bel2ayoub@example.com>
2023-09-28 14:53:32 -0700

    bitman

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * clear_bit - sets the value of a bit to 0 for index
+ * clear_bit - sets the value of a bit to 0 at a given index
  * @n: number to clear bit in
  * @index: index of bit to clear
- * Return: 1 if success -1 if error
+ * Return: 1 if success, -1 if error
  */
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int m = 1;
+	unsigned long int mask = 1;
 
 	if (index > 63)
 		return (-1);
 	mask <<= index;
-	if (m & *n)
-		*n ^= m;
+	if (mask & *n)
+		*n ^= mask;
 	return (1);
 }
-
